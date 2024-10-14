f2ca72a764d769696c957d6877e3a29e75e8491b
Gagandeep Jhanji <103517339+gagankj@users.noreply.github.com>
2024-10-14 17:42:07 +0530

    Updated

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const fs=require("fs")
 const app=express()
 
 app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
 
 app.get("/",(req,res)=>{
     res.render("Home")
@@ -10,6 +11,21 @@ app.get("/",(req,res)=>{
 
 app.get("/create",(req,res)=>{
     res.render("Create")
+})
+const date=new Date()
+const day=date.getDate()
+// console.log(day);
+const month=date.getMonth()+1
+const year=date.getFullYear()
+const fn=`${day}-${month}-${year}.txt`
+// console.log(fn);
+
+app.post("/create",(req,res)=>{
+    fs.writeFile(`files/${fn}`,req.body.details,(err)=>{
+        if (err) throw err
+        // console.log(req);
+    })
+    res.redirect("/")
 })
 
 app.listen(3000,()=>{
